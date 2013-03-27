76dc3ead762ce9f27bad6d18649a9f686c2d378a
Giorgio <imatesiu@gmail.com>
2013-03-27 18:27:37 +0100

    all

--- sr/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ sr/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,10 +2,34 @@
 //
 
 #include "stdafx.h"
+#using <System.Xml.dll>
+#include <iostream>
+
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	System::Xml::XmlReader ^reader = System::Xml::XmlReader::Create("C:\\TabellaOrario.xml");
+	while (reader->ReadToFollowing("treno")){
+		System::String ^trn = reader->GetAttribute("id");
+		while (reader->ReadToFollowing("stazione")){
+			System::String ^nomestazione = reader->GetAttribute("id");
+			reader->ReadToFollowing("arrivo");
+			System::String ^trn2 = reader->ReadString();
+			reader->ReadToFollowing("partenza");
+			System::String ^trn3 = reader->ReadString();
+
+			reader->ReadToFollowing("binarioprogrammato");
+			System::String ^trn4 = reader->ReadString();
+			reader->ReadToFollowing("latoaperturaporteprogrammato");
+			System::String ^trn5 = reader->ReadString();
+			
+			System::Console::WriteLine(trn+nomestazione+trn2+trn3+trn4+trn5);
+			System::Console::WriteLine();
+
+
+		}
+	}
 	return 0;
 }
 
